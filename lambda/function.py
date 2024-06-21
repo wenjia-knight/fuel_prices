552d3fff68f55c43c474f14b00004ef6179319c5
Wenjia Knight <wenjia.knight@hippodigital.co.uk>
2024-06-21 10:49:44 +0100

    amended function

--- lambda/function.py
+++ lambda/function.py
@@ -24,7 +24,7 @@ def extract_retailer(url):
 
 def download(url):
     try:
-        response = requests.get(url,timeout=30)
+        response = requests.get(url,timeout=2)
         response.raise_for_status()
 
         data = response.json()
